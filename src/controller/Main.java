7add85cd725af68cfea4721a427d0db168cb2903
Makai Martinez <makaimartinez.edu@gmail.com>
2023-05-17 08:27:16 -0700

    displays all room types.

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -7,9 +7,7 @@ package Controller;
 import javax.swing.JFrame;
 
 public class Main {
-
     public static void main(String[] args) {
-
 
         JFrame myWindow = new JFrame();
         myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

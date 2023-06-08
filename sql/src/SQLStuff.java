79660713f1efbd96e278927a2aa5824dd553d70e
calebkrauter <krautercaleb@gmail.com>
2023-06-08 15:23:44 -0700

    Add setter and getter to get and update database info.

--- sql/src/SQLStuff.java
+++ sql/src/SQLStuff.java
@@ -1,9 +1,11 @@
 import org.sqlite.SQLiteDataSource;
 
+import java.lang.reflect.Type;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class SQLStuff {
     private SQLiteDataSource myDS;
@@ -17,6 +19,7 @@ public class SQLStuff {
         updateTableWithQuery(initialQuery);
         fillTable();
         displayTable();
+        printTestData();
     }
     private void setupTable() {
         initialQuery = "CREATE TABLE IF NOT EXISTS monsterData ( " +
@@ -102,15 +105,65 @@ public class SQLStuff {
 
     }
 
+    ArrayList<ArrayList<Object>> myMonsters;
+    ArrayList<Object> myMonsterStats;
+    // These fields are made public because they are codes
+    // that are intended to be used outside of this class. They have a single
+    // purpose which is clearly defined as being an index to arraylists.
+    // Because they are necessary outside of this class and misusing them would not
+    // likely result in misuse of the program functionality, they are made public to be
+    // used more conveniently outside of this class.
+    public static final int MONSTER_TYPE = 0;
+    public static final int IMAGE_FILE = 1;
+    public static final int HIT_CHANCE = 2;
+    public static final int MIN_DAMAGE = 3;
+    public static final int MAX_DAMAGE = 4;
+    public static final int DEFAULT_HEALTH = 5;
+    public static final int GREMLIN = 0;
+    public static final int SKELTON = 1;
+    public static final int OGRE = 2;
+
+    private void monsterData() {
+        for (ArrayList<Object> monster: myMonsters) {
+            System.out.println();
+            for (Object stat: monster) {
+                System.out.print(stat + ", ");
+            }
+        }
+    }
+
+    public void setMonsterData(int theMonsterCode, int theStatCode, Object theStat) {
+        myMonsters.get(theMonsterCode).set(theStatCode, theStat);
+    }
+    public Object getMonsterData(int theMonsterCode, int theStatCode) {
+        return myMonsters.get(theMonsterCode).get(theStatCode);
+    }
+
+    // This method is to test that the correct data is being accessed acurately.
+    public void printTestData() {
+        System.out.println();
+        System.out.println();
+
+        String theStat = getMonsterData(SKELTON, MIN_DAMAGE).toString();
+        System.out.println("Stat from getter SKELTON, MIN_DAMAGE : " + theStat);
+
+        System.out.println();
+        System.out.println();
+        setMonsterData(SKELTON, MIN_DAMAGE, 10);
+        theStat = getMonsterData(SKELTON, MIN_DAMAGE).toString();
+        System.out.println("updated stat SKELTON, MIN_DAMAGE, : " + theStat);
+    }
     private void displayTable() {
         System.out.println("TABLE");
         initialQuery = "SELECT * FROM monsterData";
-
+        myMonsters = new ArrayList<>();
         try (Connection connection = myDS.getConnection();
             Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(initialQuery);
             System.out.println("MONSTER_TYPE, IMAGE_FILE, HIT_CHANCE, MIN_DAMAGE, MAX_DAMAGE, DEFAULT_HEALTH");
+            int theMonster = 0;
             while (resultSet.next()) {
+                myMonsterStats = new ArrayList<>();
                 String monsterType = resultSet.getString("MONSTER_TYPE");
                 String imageFile = resultSet.getString("IMAGE_FILE");
                 String hitChance = resultSet.getString("HIT_CHANCE");
@@ -118,6 +171,14 @@ public class SQLStuff {
                 String maxDamage = resultSet.getString("MAX_DAMAGE");
                 String defaultHealth = resultSet.getString("DEFAULT_HEALTH");
 
+                myMonsterStats.add(monsterType);
+                myMonsterStats.add(imageFile);
+                myMonsterStats.add(hitChance);
+                myMonsterStats.add(minDamage);
+                myMonsterStats.add(maxDamage);
+                myMonsterStats.add(defaultHealth);
+
+                myMonsters.add(myMonsterStats);
                 System.out.print(
                         monsterType + ", "
                         + imageFile + ", "
@@ -126,9 +187,11 @@ public class SQLStuff {
                         + maxDamage + ", "
                         + defaultHealth);
                 System.out.println();
+                theMonster++;
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        monsterData();
     }
 }
